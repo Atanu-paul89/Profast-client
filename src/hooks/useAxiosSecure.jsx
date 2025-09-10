8162b5153c5afbd09c9e733e3825c492896d9b02
shatadru5689 <shatadru5689@gmail.com>
2025-09-10 22:44:55 +0600

    added

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React from 'react';
+import Swal from 'sweetalert2';
 
 const axiosSecure = axios.create({
   baseURL: 'http://localhost:5000/',
@@ -13,8 +14,38 @@ axiosSecure.interceptors.request.use((config) => {
   }
   return config;
 });
- // ----- x jwt logic ended x ----- // 
 
+// ✅ Response interceptor: catch 401/403
+axiosSecure.interceptors.response.use(
+  response => response,
+  error => {
+    const status = error.response?.status;
+
+    if (status === 401 || status === 403) {
+      // Clear token
+      localStorage.removeItem("access-token");
+     
+
+      // Optional: show alert
+      Swal.fire({
+        icon: 'warning',
+        iconColor: '#CAEB66',
+        title: 'Session Expired',
+        text: 'Please log in again to continue.',
+        confirmButtonText: 'Sign In',
+        confirmButtonColor: '#03373D',
+        allowOutsideClick: false,
+      });
+
+       window.location.href = '/auth/signin';
+
+
+    }
+
+    return Promise.reject(error);
+  }
+);
+// ----- x jwt logic ended x ----- // 
 
 const useAxiosSecure = () => {
   return axiosSecure;
